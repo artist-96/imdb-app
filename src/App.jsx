0ee1022fd5553b2190964b3e36a5b808aa0d5645
Joseph Ali <146102160+artist-96@users.noreply.github.com>
2024-12-12 00:46:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
         <Routes>
 
-          <Route path='/home' element={
+          <Route path='/' element={
             <>
               <Banner />
               <Movies />
